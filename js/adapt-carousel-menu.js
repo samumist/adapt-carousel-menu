bbf3cbfbeea3286a70b11e05c8b382bd179d4368
Mike Stevens <mike.stevens@vubiz.com>
2016-10-13 09:05:06 -0400

    updated page levels

--- js/adapt-carousel-menu.js
+++ js/adapt-carousel-menu.js
@@ -51,7 +51,11 @@ define([
             var nthChild = this.model.get("_nthChild");
             var minus150per = 150 - this.model.get("_nthChild") * 100;
             var add80px = this.model.get("_nthChild") * 80 - 80;
-            var add40px = 30 - this.model.get("_nthChild") * 40;
+            var add40px = 32 - this.model.get("_nthChild") * 40;
+            var add40pxB = 64 - this.model.get("_nthChild") * 40;
+            var add40pxC = 96 - this.model.get("_nthChild") * 40;
+            var elevenplus = add40px * 2 + this.model.get("_nthChild") * 18.35;
+            var elevenplusB = add40px * 2 + this.model.get("_nthChild") * 38.93;
             var nthdivid = 100 / nthChild;
             if (graphic && graphic.src && graphic.src.length > 0) {
                 this.$el.imageready(_.bind(function() {
@@ -63,11 +67,54 @@ define([
             $('.menu-container').css({'width' : '' + this.model.get("_nthChild") + '00%','display' : 'inline-block','position' : 'relative'});
             $('.nth-child-'+ nthChild ).css({'background' : 'url('+ graphic.src +')'});
             $('.nth-child-'+ nthChild + ' .menu-item-inner').css({'left' : '' + minus150per + '%', 'margin-left' : '' + add80px + 'px'});
-            $('.duration-bar' ).css({'margin-left' : add40px + 'px'});
             $('.menu-item' ).css({'width' : nthdivid + '%', 'height' : $( window ).height() + 'px'});
+            $('.menu-item.nth-child-'+ nthChild + ' .numberspot').text('' + nthChild );
             $(window).resize(function() {
                 $('.menu-item' ).css({'width' : nthdivid + '%', 'height' : $( window ).height() + 'px'});
             });
+
+            var totalnthchild = $('.duration-bar').length;
+            
+            if (totalnthchild <= 10) {
+                $('.duration-bar').css({'margin-left' : add40px + 'px'});
+            } else if (totalnthchild <= 20) {
+                $('.duration-bar').css({'margin-left' : add40pxB / 2 + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(10)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(11)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(12)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(13)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(14)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(15)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(16)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(17)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(18)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(19)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+            } else if (totalnthchild <= 30) {
+                $('.duration-bar').css({'margin-left' : add40pxC / 3 + 'px','bottom' : '160px'});
+                $('.duration-bar:eq(10)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(11)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(12)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(13)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(14)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(15)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(16)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(17)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(18)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(19)').css({'margin-left' : elevenplusB + 'px','bottom' : '80px'});
+                $('.duration-bar:eq(20)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(21)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(22)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(23)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(24)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(25)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(26)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(27)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(28)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+                $('.duration-bar:eq(29)').css({'margin-left' : elevenplus + 'px','bottom' : '0px'});
+            } else if (totalnthchild >= 31) {
+                alert('This Menu Only Allows 30 Pages! Please use the BoxMenu Otherwise...');
+            }
+            
         },
 
         onClickMenuItemButton: function(event) {
